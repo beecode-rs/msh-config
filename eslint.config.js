3efe17edfcdc50e6b24078ebedb78401256d0717
milosbugarinovic <milos.bugarinovic@gmail.com>
2026-02-16 18:22:32 +0100

    fix:  lint issue

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,17 @@ import eslintNode from './src/eslint-config.mjs'
 
 export default [
 	{
-		ignores: ['.base-frame-template', '.idea', '.semaphore', 'coverage', 'node_modules', 'resource', '.*.js', '.*.json','.history'],
+		ignores: [
+			'.base-frame-template',
+			'.idea',
+			'.semaphore',
+			'coverage',
+			'node_modules',
+			'resource',
+			'.*.js',
+			'.*.json',
+			'.history',
+		],
 	},
 	...eslintNode,
 ]
